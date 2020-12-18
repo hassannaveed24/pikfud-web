70dce703f361bea8efc403dcc05ce6427a1d877b
abdullah2011-gif <60196750+abdullah2011-gif@users.noreply.github.com>
2020-12-19 03:55:44 +0500

    Update server.js

--- client/src/utils/server.js
+++ client/src/utils/server.js
@@ -1,19 +1,25 @@
 import axios from "axios";
-export const baseURL = "https://pikfudbackend.herokuapp.com";
+export const baseURL = "http://18.195.35.126";
 
 export const instance = axios.create({
-    baseURL,
-    headers: { "Content-Type": "application/json" },
+  baseURL,
+  headers: { "Content-Type": "application/json" },
 });
 
-export const getJobs = () => instance.get("/admin/jobs").then((res) => res.data);
+export const getJobs = () =>
+  instance.get("/admin/jobs").then((res) => res.data);
 export const getJob = async ({ queryKey }) => {
-    const [key, params] = queryKey;
-    return instance.get(`/admin/jobs/single?_id=${params._id}`).then((res) => res.data);
+  const [key, params] = queryKey;
+  return instance
+    .get(`/admin/jobs/single?_id=${params._id}`)
+    .then((res) => res.data);
 };
 
-export const getBlogs = () => instance.get("/admin/blogs").then((res) => res.data);
+export const getBlogs = () =>
+  instance.get("/admin/blogs").then((res) => res.data);
 export const getBlog = async ({ queryKey }) => {
-    const [key, params] = queryKey;
-    return instance.get(`/admin/blogs/single?_id=${params._id}`).then((res) => res.data);
+  const [key, params] = queryKey;
+  return instance
+    .get(`/admin/blogs/single?_id=${params._id}`)
+    .then((res) => res.data);
 };
